8e7d0955b608b8e76b62a65218ab56cda7d77637
Derek Goldstone <derekgoldstone@yahoo.com>
2019-07-01 22:52:10 -0700

    Added post route to calculate best match for friends score. First iterated through friends array of objects to obtain each friend score. Then nested inner loop iterates through friend score and user score to calculate absolute difference using Math.abs.

--- server.js
+++ server.js
@@ -25,8 +25,31 @@ app.get("/api/friends", function(req,res) {
     res.json(friends);
 });
 
-
-
+app.post("/api/friends", function(req,res) {
+    var survey = req.body;
+    var scores = survey.scores;
+    console.log(scores);
+    var match = {
+        name: "",
+        photo: "",
+        difference: 100
+    };
+    
+    for (var i = 0; i < friends.length; i++) {
+        //outer loop iterates through friends array of objects to obtain each friend score
+        var currentDiff = 0;
+        for (var j = 0; j < friends[i].scores.length; j++) {
+            //inner loops iterates through friend score and user score to calculate absolute difference using Math.abs
+            currentDiff += Math.abs(friends[i].scores[j] - scores[j]);
+        }
+        if (currentDiff <= match.difference) {
+            match.name = friends[i].name;
+            match.photo = friends[i].photo;
+            match.difference = currentDiff;
+        }
+    };
+    res.json(match);
+});
 
 
 app.listen(PORT, function() {
