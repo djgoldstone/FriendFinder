a1a7ec6b79824739114ab526583d17d6aa3784ba
Derek Goldstone <derekgoldstone@yahoo.com>
2019-07-01 20:50:29 -0700

    Created on-click listener for submit button on survey.html. Inputs from each field are stored in variables which are then populated into an object. Ajax post method then sends data to the friends api.

--- server.js
+++ server.js
@@ -11,3 +11,11 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
