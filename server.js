091ec508a02a018fa9bc5fecc9fd5a242367615e
Derek Goldstone <derekgoldstone@yahoo.com>
2019-07-01 20:13:04 -0700

    Added dependencies and initialized express packages then set PORT to 8080 in server.js. Created dummy friends objects containing mock data in friends.js. Created basic HTML files for survey and home pages using bootstrap and font awesome.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 var path = require("path");
+//dependencies/npm packages
 
 var app = express();
+//a variable assigned to express package methods
 
 var PORT = process.env.PORT || 8080;
+//variable assigned to the initial port
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
